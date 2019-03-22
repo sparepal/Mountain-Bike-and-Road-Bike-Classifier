b515c293c6c4b4ec826b744b036df24bd117ba5d
Sai Vikhyat <sparepal@uncc.edu>
2019-03-22 15:28:36 -0400

    Update train.py

--- train.py
+++ train.py
@@ -45,17 +45,11 @@ train_datagen = ImageDataGenerator(rescale = 1./255,
                                    zoom_range = 0.2,
                                    horizontal_flip = True)
 
-test_datagen = ImageDataGenerator(rescale = 1./255)
-
 training_set = train_datagen.flow_from_directory(r'C:\Files\Datasets\bikes\train_set',
                                                  target_size = (64, 64),
                                                  batch_size = 32,
                                                  class_mode = 'binary')
 
-test_set = test_datagen.flow_from_directory(r'C:\Files\Datasets\bikes\test_set',
-                                            target_size = (64, 64),
-                                            batch_size = 32,
-                                            class_mode = 'binary')
 #Saving results to Tensorboard 
 #In terminal enter the command
 #$ python -m tensorboard.main --logdir=C:\Users\vikhy\Desktop\logs
